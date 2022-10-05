1aa77e3620f81c22167ba8a53823f8a4fe9ae033
Kumarvlsi <113289567+Kumarvlsi@users.noreply.github.com>
2022-10-05 22:38:23 +0530

    Delete Assignmen-2 Devarajan directory

--- Assignmen-2 Devarajan/db.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-con=sqlite3.connect("lists.db")
-print("successfullly connected");
-
-con.execute("CREATE TABLE lists (name TEXT,email TEXT,phone TEXT ,password TEXT)")
-
-print("table created")
-con.close()
